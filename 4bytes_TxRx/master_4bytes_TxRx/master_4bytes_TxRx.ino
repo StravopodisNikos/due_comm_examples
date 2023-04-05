0cdad54119788952df4c3a666528a5a0645b98e2
SyrosRoboticsResearch1 <stravnik.rob@gmail.com>
2023-04-05 22:38:45 +0300

    Master works, but gets no response, so prints again the command sent. Slave code need work! It is chaos

---
+++ 4bytes_TxRx/master_4bytes_TxRx/master_4bytes_TxRx.ino
@@ -0,0 +1,111 @@
+#include <SPI.h>
+/*
+ * ***********************************************************
+ * ********************       MASTER       *******************
+ * ***********************************************************
+ * Simple TEST code for Ovidius SMM Project. Arduino DUE is con-
+ * figured as Master for SPI Communication. Summary:
+ * 
+ *  When MASTER (DUE1) sends a specific (float) value (32bit),
+ *  the SLAVE(DUE2) responds accordingly. Main difference is
+ *  that the transaction is executed by defining a Union and
+ *  passing the 2 16bit parts of the float to be transfered.
+ *  
+ * [5-4-23] Author: Stravopodis Nikos
+ */
+#define SERIAL_BAUD1 250000
+#define CSPIN_EXT   10
+#define SPI0_MISO   25
+#define SPI0_MOSI   26
+#define SPI0_SPCK   27
+#define SPI0_NPCS0  10
+#define CMD_LED_ON_F  14.3785f // 0x41660e56 [HEX]
+#define CMD_LED_OFF_F 96.7854f // 0x42c191aa [HEX]
+#define STATE_LED_ON  64.7836f // 0x42819134 [HEX]
+#define STATE_LED_OFF 74.2418f // 0x42947bcd [HEX]
+#define RESPONSE_TIMEOUT 100 // [ms]
+
+typedef union{
+  float cmd_f;
+  uint16_t cmd_parts[2];
+} cmd_un_t;
+
+cmd_un_t cmd2send;
+
+unsigned long time_start_millis;
+bool timeout_error;
+SPISettings spi_setting(SPI_CLOCK_DIV2, MSBFIRST, SPI_MODE2);
+float SlaveResponseReceived_f;
+
+void setupMaster(uint8_t cs_pin)
+{
+  pinMode(cs_pin,OUTPUT);
+  digitalWrite(cs_pin, HIGH);
+  SPI.begin();
+
+  timeout_error = false;
+  SlaveResponseReceived_f = 0;
+  return;
+}
+
+void sendReadCmd2SlaveWait(uint8_t cs_pin, cmd_un_t cmd, uint16_t * response, float & response_f)
+{
+  //begins Transaction and waits for the full response, or timeouts
+  bool response_received = false;
+  timeout_error = false;  
+  SPI.beginTransaction(spi_setting);
+  digitalWrite(cs_pin, LOW);
+  time_start_millis = millis();
+  while ( (!response_received) && (!timeout_error) )
+  {
+    *response     = SPI.transfer16(cmd.cmd_parts[0]);
+    *(response+1) = SPI.transfer16(cmd.cmd_parts[1]);
+    
+    if ( (millis() - time_start_millis) > RESPONSE_TIMEOUT)
+    {
+      timeout_error = true;
+      Serial.println("TIMEOUT ERROR!");
+    }
+    if (response > 0)
+    {
+      response_f = cmd.cmd_f;
+      response_received = true;
+      digitalWrite(cs_pin, HIGH);
+      SPI.endTransaction();
+    }
+  }
+
+  if (!response_received)
+  {
+      digitalWrite(cs_pin, HIGH);
+      SPI.endTransaction();    
+  }
+
+  return;
+}
+
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD1);
+  setupMaster((uint8_t) SPI0_NPCS0);
+  
+  Serial.println("MASTER started sending data...");
+  delay(500);
+}
+
+void loop() {
+  uint16_t slaveResponseAr[2];
+  cmd2send.cmd_f = CMD_LED_ON_F;
+  sendReadCmd2SlaveWait( (uint8_t)  SPI0_NPCS0, cmd2send, slaveResponseAr, SlaveResponseReceived_f);
+  Serial.println("MASTER SENT: CMD_LED_ON_F"); 
+  Serial.print("MASTER RECEIVED: "); Serial.println(SlaveResponseReceived_f,DEC);
+  delay(1000);
+
+  cmd2send.cmd_f = CMD_LED_OFF_F;
+  sendReadCmd2SlaveWait( (uint8_t)  SPI0_NPCS0, cmd2send, slaveResponseAr, SlaveResponseReceived_f);
+  Serial.println("MASTER SENT: CMD_LED_OFF_F"); 
+  Serial.print("MASTER RECEIVED: "); Serial.println(SlaveResponseReceived_f,DEC);
+  delay(1000);  
+
+  delay(1000);
+}
---
+++ 4bytes_TxRx/slave_4bytes_TxRx/slave_4bytes_TxRx.ino
@@ -0,0 +1,218 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <sam.h>
+/*
+ * ***********************************************************
+ * ********************       SLAVE       ********************
+ * ***********************************************************
+ * Simple TEST code for Ovidius SMM Project. Arduino DUE is con-
+ * figured as Slave for SPI Communication. Summary:
+ * 
+ *  When MASTER (DUE1) sends a specific (float) value (32bit),
+ *  the SLAVE(DUE2) responds accordingly. Main difference is
+ *  that the transaction is executed by defining a Union and
+ *  passing the 2 16bit parts of the float to be transfered.
+ * 
+ * [5-4-23] Author: Stravopodis Nikos
+ */
+#define SERIAL_BAUD1 250000
+#define CSPIN_EXT   10
+#define SPI0_MISO   25
+#define SPI0_MOSI   26
+#define SPI0_SPCK   27
+#define SPI0_NPCS0  10
+#define CMD_LED_ON_F  14.3785f // 0x41660e56 [HEX]
+#define CMD_LED_OFF_F 96.7854f // 0x42c191aa [HEX]
+#define STATE_LED_ON  64.7836f // 0x42819134 [HEX]
+#define STATE_LED_OFF 74.2418f // 0x42947bcd [HEX]
+#define LEDpin      4
+
+uint8_t CMD_Received;
+uint8_t last_CMD_Transit;
+float __CMD_Received_f;
+volatile uint32_t __slave_status;
+volatile long unsigned int * data_in;
+volatile bool __data_received_flag;
+volatile bool __overs_flag;
+volatile bool __modfault_flag;
+volatile bool __DataKeepComing;
+uint16_t receivedData = 0;
+volatile uint8_t pos;
+volatile uint16_t __PacketReceived[2];
+bool react2ReceivedCmd;
+
+typedef union{
+  float res_f;
+  uint16_t res_parts[2];
+} res_un_t;
+res_un_t res2send;
+uint16_t packet_received_buffer[3];
+
+// [1-4-223] Author Stravopodis N.
+void setupSlave(uint8_t cs_pin)
+{
+  pinMode(SPI0_MISO, OUTPUT);
+  // SPI Control Register(P.32.8.1/p.693): Enable the SPI to transfer and receive data
+  SPI0->SPI_CR  = 0x00000001; //SPI_CR_SPIEN  
+  // SPI Mode Register(P.32.8.2/p.694): Enable the SPI in Slave Mode
+  SPI0->SPI_WPMR = 0x00000000; // Write Protection Disabled - NO KEY -> Must be Disabled to write in MR!
+  SPI0->SPI_MR  = 0x00000000; //
+  // SPI Interrupt Enable Register(P.32.8.6/p.700): Enable the Interrupts
+  SPI0->SPI_IER = 0x00000001; // Enable RDRF(Receive Data Register Full Interrupt Enable) 
+  NVIC_EnableIRQ(SPI0_IRQn); // FP.10.20.10.1 p.164
+  // SPI Chip Select Register (P.32.8.9/p.703)
+  SPI0->SPI_CSR[0] = SPI_CSR_NCPHA | SPI_DLYBCT(10,8) | SPI_CSR_BITS_8_BIT| SPI_CSR_SCBR(1);
+  __DataKeepComing     = true;
+  __data_received_flag  = false;
+  __overs_flag          = false;
+  __modfault_flag       = false;
+  pos = 0;
+  return;
+}
+
+void respond2masterON()
+{
+  res2send.res_f = (float) STATE_LED_ON;
+  REG_SPI0_TDR = res2send.res_parts[0];
+  while (!(SPI0->SPI_SR & SPI_SR_TDRE));
+  REG_SPI0_TDR = res2send.res_parts[1];
+  while (!(SPI0->SPI_SR & SPI_SR_TDRE));
+  return;
+}
+void respond2masterOFF()
+{
+  res2send.res_f = (float) STATE_LED_OFF;
+  REG_SPI0_TDR = res2send.res_parts[0];
+  while (!(SPI0->SPI_SR & SPI_SR_TDRE));
+  REG_SPI0_TDR = res2send.res_parts[1];
+  while (!(SPI0->SPI_SR & SPI_SR_TDRE));
+  return;
+}
+void ChangeLEDstate2ON()
+{
+  //__led_state = !__led_state;
+  digitalWrite(LEDpin, HIGH); delay(500);
+  return;
+}
+void ChangeLEDstate2OFF()
+{
+  //__led_state = !__led_state;
+  digitalWrite(LEDpin, LOW); delay(500);
+  return;
+}
+
+void SPI0_Handler()
+{
+  if (SPI_SR_OVRES == HIGH) // Check if OVERES flag is risen
+  {
+    __slave_status = SPI0->SPI_SR;
+    __overs_flag = true;
+  }
+  if (SPI_SR_MODF == HIGH) // Check if Mode Fault Error flag is risen
+  {
+    __modfault_flag = true;
+  }  
+  if (SPI_SR_TDRE == LOW)
+  {
+    // Based on SPI Status Register(P.32.8.5/p.698): Data has been written to SPI_TDR but
+    // yet not transferred to the serializer. SO => doesn't read again BUT sends the data!
+    Serial.println("NO TRANSMITTED DATA");
+  }
+  else
+  {
+    if ((!__overs_flag) && (SPI0->SPI_SR & SPI_SR_RDRF)) 
+    {  
+      if (__DataKeepComing)
+      {
+        __PacketReceived[pos] = SPI0->SPI_RDR; // pos = 0
+        pos++;
+        if (pos >=2)
+        {
+          __data_received_flag = true;
+        }
+        else
+        {
+          __DataKeepComing = true;
+          __data_received_flag = false;
+        }
+      }
+      else
+      {
+        __PacketReceived[pos] = SPI0->SPI_RDR; // pos = 1
+        __DataKeepComing = true;
+        __data_received_flag = false;
+      }
+      if (__data_received_flag)
+      {
+        __DataKeepComing = false;
+        SPI0->SPI_RDR; // Clear the RDR
+      }
+    }
+    else
+    {
+      Serial.println("NO UNREAD DATA");
+    }
+  }
+  
+  return;
+}
+
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD1);
+  SPI.begin(SPI0_NPCS0);
+  SPI.setBitOrder(MSBFIRST);
+  setupSlave((uint8_t) SPI0_NPCS0);
+  SPI.attachInterrupt();
+  pinMode(LEDpin, OUTPUT);
+  digitalWrite(LEDpin, LOW); // Make sure Led is OFF
+  ChangeLEDstate2ON(); delay(500); ChangeLEDstate2OFF(); delay(500);
+ 
+  Serial.println("Slave starts listening to master...");
+  react2ReceivedCmd = false;
+}
+
+void loop() 
+{
+  if (__modfault_flag)
+  {
+    // Do nothing...
+    __modfault_flag = false;
+  }
+  if (__overs_flag)
+  {
+    // Do nothing...
+    __overs_flag = false;
+  }  
+  if (__data_received_flag)
+  {
+    packet_received_buffer[2] = (uint16_t) 0;
+    for (size_t i = 0; i < sizeof(res2send); i++)
+    {
+      res2send.res_parts[i] = __PacketReceived[i];
+    }
+    __CMD_Received_f = res2send.res_f;
+
+    pos = 0;    
+    __data_received_flag = false;
+    react2ReceivedCmd = true;
+  }
+
+  if (react2ReceivedCmd)
+  {
+    if (__CMD_Received_f == CMD_LED_ON_F)
+    {
+      ChangeLEDstate2ON();
+      respond2masterON();
+    }
+    if (__CMD_Received_f == CMD_LED_OFF_F)
+    {
+      ChangeLEDstate2OFF();
+      respond2masterOFF();
+    }
+    __DataKeepComing = true;
+    react2ReceivedCmd = false;
+  }
+
+  delay(10);
+}
