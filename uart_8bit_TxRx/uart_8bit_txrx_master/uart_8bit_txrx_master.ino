a48f9c29295694a6932857d74af0d4afb5fae29d
SyrosRoboticsResearch1 <40363322+StravopodisNikos@users.noreply.github.com>
2023-04-22 22:09:46 +0300

    Added the Dynamixel Serial port handling, since this will be used for a mega project built on the Dxl Shield
    Confirmed that hardware serial port 3 of the DUE works when Serial0 is used for the dxl communication

--- uart_8bit_TxRx/uart_8bit_txrx_master/uart_8bit_txrx_master.ino
+++ uart_8bit_TxRx/uart_8bit_txrx_master/uart_8bit_txrx_master.ino
@@ -1,7 +1,12 @@
+#include <Dynamixel2Arduino.h>
 
-#define SERIAL0_BAUD1 250000
+#define SERIAL0_BAUD1 115200
 #define SERIAL3_BAUD  115200
 #define SERIAL3_BAUD1 1000000
+
+#define MAX_PACKET_BITS 10
+
+const uint8_t DXL_DIR_PIN = 2;
 
 /*
  * DUE(master) -> Serial0 -> PC
@@ -22,9 +27,24 @@
 
 uint8_t data2send;
 uint8_t response;
+uint16_t data2send16;
+
+uint16_t merge2Bytes(uint8_t HighB, uint8_t LowB) {
+  return ((uint16_t) HighB << 8 ) | LowB;
+}
 
 void setByte(uint8_t SetByteCmd) {
   Serial3.write(SetByteCmd);
+  return;
+}
+
+void set2Bytes(uint16_t SetByteCmd16) {
+  uint8_t LowByte  = SetByteCmd16 & 0xFF;
+  uint8_t HighByte = (SetByteCmd16 >> 8) & 0xFF;
+  
+  Serial3.write(HighByte);
+  while(Serial3.availableForWrite()<MAX_PACKET_BITS);
+  Serial3.write(LowByte);
   return;
 }
 
@@ -34,17 +54,32 @@ uint8_t getByte(uint8_t GetByteCmd) {
     return Serial3.read();
 }
 
-void setup() {
-  Serial.begin(SERIAL0_BAUD1);
-  Serial3.begin(SERIAL3_BAUD1);
+uint16_t get2Byte(uint8_t Get2BytesCmd) {
+    while(Serial3.availableForWrite()<MAX_PACKET_BITS);
+    Serial3.write(Get2BytesCmd);
+    while(!Serial3.available());
+    uint8_t HighByteReceived = Serial3.read();
+    while(!Serial3.available());
+    uint8_t LowByteReceived = Serial3.read();
+    uint16_t Packet16Received = merge2Bytes(HighByteReceived, LowByteReceived);
+    return Packet16Received;
+}
 
-  while (!Serial); //wait for the Serial Monitor to pop
-  Serial.println("MASTER STARTED");
+Dynamixel2Arduino dxl(Serial, DXL_DIR_PIN);
+
+void setup() {
+  SerialUSB.begin(SERIAL0_BAUD1);
+  Serial3.begin(SERIAL3_BAUD);
+
+  while (!SerialUSB); //wait for the Serial Monitor to pop
+  SerialUSB.println("MASTER STARTED");
 
 }
 
 void loop() {
   setByte(CMD_LED_ON_1); delay(1000);
   response = getByte(CMD_LED_OFF_1); delay(1000);
-  Serial.print("SLAVE RESPONDED: "); Serial.println(response,BIN);
+  SerialUSB.print("SLAVE RESPONDED: "); SerialUSB.println(response,BIN);
+
+  
 }
